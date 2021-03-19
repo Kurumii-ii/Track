9ee48f3670ee40db19c09e1df21051ae3d6cbcbf
GG-Henry1833 <69804977+GG-Henry1833@users.noreply.github.com>
2021-03-19 20:13:38 +0800

    Update Track.py

--- Track.py
+++ Track.py
@@ -1,6 +1,6 @@
 import pygeoip
 
 gip = pygeoip.GeoIP("GeoLiteCity.dat")
-res = gip.record_by_addr('')
+res = gip.record_by_addr(input(''))
 for key, val in res.items():
     print('%s : %s' % (key,val))
